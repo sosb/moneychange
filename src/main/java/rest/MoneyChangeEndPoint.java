ab6bb16c60c0223448f3d1f36696e47f2658033c
sos.bence <sos.bence@virgo.hu>
2017-07-30 15:35:03 +0200

    vol4

--- src/main/java/rest/MoneyChangeEndPoint.java
+++ src/main/java/rest/MoneyChangeEndPoint.java
@@ -5,25 +5,22 @@ import moneychange.Currency;
 import moneychange.CurrencyService;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 
+
 @Path("/change")
-public class MoneyChangeEndPoint {
+public class MoneyChangeEndPoint{
 
     private CurrencyService currencyService;
 
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getThis(){
+    public Response getAllData(){
 
-        return Response.ok(currencyService.changeCurrency(Currency.HUF_AFTER2008, 35_000)).build();
+        return Response.ok(currencyService.changeCurrency(Currency.HUF_AFTER2008, 60_000)).build();
 
     }
 
@@ -31,4 +28,5 @@ public class MoneyChangeEndPoint {
     public void setCurrencyService(CurrencyService currencyService) {
         this.currencyService = currencyService;
     }
+
 }
